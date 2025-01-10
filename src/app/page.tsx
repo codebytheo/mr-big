4dc0e0fde39701bd731bf680fd85f6128c138764
codebytheo <theocode1@gmail.com>
2025-01-10 23:33:47 +0800

    add music

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,13 @@ import BriefHistory from "@/pages/brief-history";
 import LineUpChange from "@/pages/line-up-change";
 import Footer from "@/pages/footer";
 import Farewell from "@/pages/farewell";
+import AudioMusic from "@/components/audio-music";
 
 export default function Home() {
   return (
     <main className="min-h-dvh overflow-x-hidden">
       <div className="noise" />
+      <AudioMusic />
       <Intro />
       <EarlyFormed />
       <BriefHistory />
---
+++ src/components/audio-music.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useRef, useState } from "react";
+import { IoVolumeHigh, IoVolumeMute } from "react-icons/io5";
+
+const AudioMusic = () => {
+  const [isButtonClick, setIsButtonClick] = useState<boolean>(false);
+  const music = useRef<HTMLAudioElement>(null)
+
+  const handleAudio = () => {
+    if(isButtonClick === true) {
+      setIsButtonClick(false)
+      music?.current?.pause()
+    }
+    if(isButtonClick === false) {
+      setIsButtonClick(true);
+      music?.current?.play();
+    }
+  }
+
+  return (
+    <div className="fixed w-screen h-full top-8 left-10">
+      <button className="btn" id="playAudio" onClick={handleAudio} type="button">
+      {isButtonClick ? (
+        <IoVolumeHigh size={25} color="white" />
+      ) : (
+        <IoVolumeMute size={25} color="white" />
+      )}
+      </button>
+      <audio id='audio-mr-big' loop={true} ref={music}>
+        <source src='/music/mr.big-green-tinted sixties mind.mp3' type='audio/mpeg' />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  )
+}
+
+export default AudioMusic;
